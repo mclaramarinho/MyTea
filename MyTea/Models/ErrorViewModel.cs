3ff3e5dba9325e8106f72d10fc6570f018793e2c
mclaramarinho <marinho.claramb@gmail.com>
2024-05-14 14:27:18 -0300

    git init

---
+++ MyTea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyTea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
