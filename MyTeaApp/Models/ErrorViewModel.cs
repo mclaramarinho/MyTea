4d3daac0b1e000b1f35a42e74c0566d77a8efcaa
mclaramarinho <marinho.claramb@gmail.com>
2024-05-15 14:33:20 -0300

    init repo

---
+++ MyTeaApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyTeaApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
