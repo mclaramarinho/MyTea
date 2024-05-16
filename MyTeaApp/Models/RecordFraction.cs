6a0e998da0d3c740dca97009822a5a8fb5b2d41f
Yasmin <yasmiin.vargass@gmail.com>
2024-05-16 13:42:06 -0300

    created model RecordFraction

---
+++ MyTeaApp/Models/RecordFraction.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MyTeaApp.Models
+{
+    public class RecordFraction
+    {
+        [Key]
+        public Guid RecordFractionID { get; set; }
+
+        //[ForeignKey("WbsID")]
+        //[Display(Name = "WBS")]
+        //public int WbsID { get; set; }
+        //public virtual WBS Wbs { get; set; }
+
+        //**** Default (?) *****
+        [Required(ErrorMessage = "{0} Required")]
+        [Display(Name = "Record Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
+        public DateTime RecordDate { get; set; }
+
+
+        [Display(Name = "Total Hours")]
+        [Required(ErrorMessage = "{0} Required")]
+        public float TotalHoursFraction { get; set; }
+
+
+        //[ForeignKey("RecordID")]
+        //public int RecordID { get; set; }
+        //public virtual Record Record { get; set; }
+    }
+}
