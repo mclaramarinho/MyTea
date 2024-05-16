f834c0d421cdebb2c67acbc4db5e99eb858fc25a
VivianeLopes <v.almeida.do.carmo@avanade.com>
2024-05-16 13:36:13 -0300

    wbs model criada

---
+++ MyTeaApp/Models/WBS.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NuGet.Protocol.Plugins;
+
+namespace MyTeaApp.Models
+{
+    public class WBS
+    {
+        [Key]
+        [Required]
+        public int WbsId { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "Minimum length for WBS code is 4 characters")]
+        [Display(Name = "Código WBS")]
+        public string? CodWbs { get; set;}
+
+        [Required(ErrorMessage = "{0} Required")]
+        public string? Description {  get; set;}
+
+        
+        [Display(Name = "Chargeble")]
+        [Required(ErrorMessage = "{0} Required")]
+        public bool? IsChargeable { get; set; }
+
+        //public ICollection<RecordFraction>
+
+
+    }
+}
