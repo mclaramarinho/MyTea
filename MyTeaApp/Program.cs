92da4d3ec598f73e5ec25cf425fa76e3ed918f55
mclaramarinho <marinho.claramb@gmail.com>
2024-05-17 23:58:37 -0300

    FEAT: created policies + removed unused code + implemented custom identity + called DbInitializer

--- MyTeaApp/Program.cs
+++ MyTeaApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MyTeaApp.Data;
+using MyTeaApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,17 +9,38 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddRoles<IdentityRole>()
-                    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+    
+    options.User.RequireUniqueEmail = true;
+
+    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//                    .AddRoles<IdentityRole>()
+//                    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddAuthorization(options =>
 {
-    options.AddPolicy("RequerPerfilAdmin",
-         policy => policy.RequireRole("Administrator"));
+    options.AddPolicy("RequireAdmin",
+         policy => policy.RequireRole("Admin"));
+
+    options.AddPolicy("ElevatedPrivileges", 
+        policy => policy.RequireRole(["Admin", "Manager"]));
 });
 
 var app = builder.Build();
@@ -45,21 +67,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+//app.MapRazorPages();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        DbInitializer.Initialize(services).Wait();
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
-    }
-}
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
