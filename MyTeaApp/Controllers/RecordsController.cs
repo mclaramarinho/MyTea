3243a128de900375fd56899994e1396d84df64e0
mclaramarinho <marinho.claramb@gmail.com>
2024-05-21 11:08:44 -0300

    FEAT: started view model implementation for record creation

--- MyTeaApp/Controllers/RecordsController.cs
+++ MyTeaApp/Controllers/RecordsController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyTeaApp.Data;
 using MyTeaApp.Models;
+using MyTeaApp.Models.ViewModels;
 
 namespace MyTeaApp.Controllers
 {
@@ -44,11 +41,47 @@ namespace MyTeaApp.Controllers
         }
 
         // GET: Records/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            return View();
-        }
+            RecordVM vm = new RecordVM();
+            vm.RecordFractions = new List<RecordFraction>();
 
+            // TODO - puxar as informações do banco de dados para montar o select
+            vm.WBS = new List<SelectListItem>()
+            {
+                new SelectListItem { Text="Ferias", Value="1" },
+                new SelectListItem { Text="Day-off", Value="2" },
+                new SelectListItem { Text="Desenvolvimento", Value="3" },
+            };
+           
+           
+            for (int i = 0; i < 60; i++)
+            {
+                vm.RecordFractions.Add(
+                    new RecordFraction()
+                  );
+            }
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult Create(RecordVM vm)
+        {
+            // TODO - Primeiro cria a record para então criar as fractins e indicar a qual record ela pertence
+            Record r = new Record()
+            {
+                //User = colocar o user logado
+
+            };
+
+            foreach (var fraction in vm.RecordFractions)
+            {
+                var temp = new RecordFraction()
+                {
+
+                };
+            }
+            return View(vm);
+        }
 
         // GET: Records/Edit/5
         public async Task<IActionResult> Edit(int? id)
