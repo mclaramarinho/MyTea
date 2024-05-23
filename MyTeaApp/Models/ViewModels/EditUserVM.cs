27c5792ca422279ff08d06b4f0f5cae4e881649c
mclaramarinho <marinho.claramb@gmail.com>
2024-05-23 11:55:14 -0300

    FEAT: added EditUser action + view models + view for admins to update user information

---
+++ MyTeaApp/Models/ViewModels/EditUserVM.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+namespace MyTeaApp.Models.ViewModels
+{
+    public class EditUserVM
+    {
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        [DisplayName("Full Name")]
+        public string? FullName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Departament")]
+        public string DepartmentId { get; set; }
+        public List<SelectListItem> Departments { get; set; }
+
+        [Required]
+        [DisplayName("Role")]
+        public string RoleName { get; set; }
+        public List<SelectListItem> Roles { get; set; }
+
+        public void Populate(User user, string role, List<Department> deps, List<IdentityRole> roles)
+        {
+            Console.WriteLine("User UID: " + user.UserID);
+            this.UserID = user.UserID;
+            this.FullName = user.FullName;
+            this.Email = user.Email;
+            this.RoleName = role;
+            this.DepartmentId = user.DepartmentId.ToString();
+
+            this.Departments = new List<SelectListItem>();
+            this.Roles = new List<SelectListItem>();
+            deps.ForEach(d => Departments.Add(new SelectListItem { Text = d.DepartmentName, Value = d.DepartmentID.ToString() }));
+            roles.ForEach(r => Roles.Add(new SelectListItem { Text = r.Name, Value = r.Name}));
+
+        }
+    }
+}
