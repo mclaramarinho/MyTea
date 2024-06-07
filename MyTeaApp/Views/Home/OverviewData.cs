98d701ee791c7292a4399f0a95cf7819d5ce01b3
mclaramarinho <marinho.claramb@gmail.com>
2024-06-06 23:22:19 -0300

    FEAT: refactored and implemented Jaque's overview info

---
+++ MyTeaApp/Views/Home/OverviewData.cs
@@ -0,0 +1,10 @@
+﻿namespace MyTeaApp.Views.Home
+{
+    public class OverviewData
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
