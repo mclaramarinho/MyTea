bafe54a405631134b81e302a8c8d2aaf99a3d97e
mclaramarinho <marinho.claramb@gmail.com>
2024-06-07 10:45:35 -0300

    FEAT: updated notification partial to be more customizable

---
+++ MyTeaApp/Models/HelperModels/NotificationModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MyTeaApp.Models.HelperModels
+{
+    public class NotificationModel
+    {
+        public string Type { get; set; }
+        public string Message { get; set; }
+        public string BgColor { get; set; }
+        public NotificationModel(string type, string? message, string? bgColor)
+        {
+            this.Type = type;
+
+            if(message == null)
+            {
+                this.Message = type == "success" ? "Success!" : type == "error" ? "Error!" : "Something went bad";
+            }
+            else
+            {
+                this.Message = message == "success" ? "Success!" : message == "error" ? "Error!" : message;
+
+            }
+
+            this.BgColor= type == "success" ? "bg-success" : type == "error" ? "bg-danger" : type == "warning" ? "bg-warning" : bgColor;
+        }
+    }
+}
